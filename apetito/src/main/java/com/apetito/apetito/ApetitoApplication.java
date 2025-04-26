2e5f9a9a0674a59da6d7dfb59b0852988e9a2ba2
Lucas Resende <lucasalves2180@gmail.com>
2025-04-26 13:16:55 -0300

    consertando target

---
+++ apetito/src/main/java/com/apetito/apetito/ApetitoApplication.java
@@ -0,0 +1,13 @@
+package com.apetito.apetito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApetitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApetitoApplication.class, args);
+	}
+
+}
